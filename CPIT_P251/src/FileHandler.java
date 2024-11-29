8baca4731685966f2ab0d643995e1d20ec98ddb1
JawaherAlsharif <>
2024-11-29 18:12:36 +0300

    Add Admin class + Main class + Filehandler class + Information class

---
+++ CPIT_P251/src/FileHandler.java
@@ -0,0 +1,31 @@
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class FileHandler {
+
+    private String fileName = "data.txt";
+
+    // Constructor
+    public FileHandler() {
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.err.println("Error initializing the file: " + e.getMessage());
+        }
+    }
+
+    // Read all data from the file method
+    
+    // Write all data to the file (overwrites the file) method
+    
+    // Append new data to the file
+
+}
